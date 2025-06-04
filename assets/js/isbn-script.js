2e378fa5ceb599a57d2af478524c0175c9bf38ed
RenoCross <renaudcrosset@gmail.com>
2025-06-04 07:35:22 +0200

    Update isbn-script.js

--- assets/js/isbn-script.js
+++ assets/js/isbn-script.js
@@ -29,8 +29,8 @@ async function fetchBookData() {
 		try {
 			let data;
 			if (api === "openlibrary") {
-				//const res = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`);
-				const res = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=details`);				
+				const res = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`);
+				//const res = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=details`);				
 				const jsonData = await res.json();
 				data = jsonData[`ISBN:${isbn}`];
 				jsonOutput.textContent += `\n[OpenLibrary]\n` + JSON.stringify(data, null, 2);
