89015cc81eccac570464230208c769570a56456d
RenoCross <renaudcrosset@gmail.com>
2025-06-06 08:32:29 +0200

    Update isbn-script.js

--- assets/js/isbn-script.js
+++ assets/js/isbn-script.js
@@ -133,13 +133,13 @@ function renderUnifiedTable(allData, req) {
 		"Classification décimale Dewey": d =>
 			d.classifications?.dewey_decimal_class 
 			|| d.details?.dewey_decimal_class 
-			|| "-",
+			|| "-"
 		//"ID": d => d.key || d.details?.key || d.id || "-"
 		//"URL": d => d.url ? `<a href="${d.url}">${d.url}</a>` || "-",
-		"Couverture": d => 
-			`<img src="${d.cover.small}" alt="cover">` 
-			||  `<img src="${d.details.thumbnail_url}" alt="cover">` 
-			|| "-"
+		//"Couverture": d => 
+		//	`<img src="${d.cover.small}" alt="cover">` 
+		//	||  `<img src="${d.details.thumbnail_url}" alt="cover">` 
+		//	|| "-"
 	};
 
 	for (const label in fields) {
